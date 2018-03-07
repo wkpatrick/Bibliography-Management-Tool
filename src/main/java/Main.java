ab6a19e84e606082ecb8520bdce08ec1ff652b86
Avery Snyder <madrauk49@gmail.com>
2018-03-06 21:23:06 -0500

    Added exit menu button functionality and publicly accessible source list.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,6 +4,8 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+import java.util.ArrayList;
+
 public class Main extends Application {
 
     public static void main(String[] args) {
@@ -16,6 +18,7 @@ public class Main extends Application {
             Parent root = FXMLLoader.load(getClass().getResource("MainWindow.fxml"));
             primaryStage.setTitle("Bibliography Management Tool");
             primaryStage.setScene(new Scene(root));
+            MainWindowController.sourceList = new ArrayList<>();
             primaryStage.show();
         }
         catch(Exception e){
--- src/main/java/MainWindowController.java
+++ src/main/java/MainWindowController.java
@@ -1,7 +1,29 @@
+import javafx.application.Platform;
 import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import java.util.List;
 
 public class MainWindowController {
+
+    public static List<Source> sourceList;
+
     public void createNewSource(ActionEvent actionEvent) {
-        System.out.println("Test!");
+        try {
+            Stage primaryStage = new Stage();
+            Parent root = FXMLLoader.load(getClass().getResource("SourceView.fxml"));
+            primaryStage.setTitle("Source Viewer");
+            primaryStage.setScene(new Scene(root));
+            primaryStage.show();
+        }
+        catch(Exception e){
+            System.out.println(e);
+        }
+    }
+    public void Close(ActionEvent actionEvent) {
+        //save current list?
+        Platform.exit();
     }
 }
